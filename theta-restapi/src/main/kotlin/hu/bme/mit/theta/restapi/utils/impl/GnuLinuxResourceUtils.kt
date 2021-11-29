0f16414f56b1994d63c5e91a3d757621eabc34ab
Levente Bajczi <bajczilevi@gmail.com>
2021-11-29 22:05:53 +0100

    Moved from using bash to sh

--- theta-restapi/src/main/kotlin/hu/bme/mit/theta/restapi/utils/impl/GnuLinuxResourceUtils.kt
+++ theta-restapi/src/main/kotlin/hu/bme/mit/theta/restapi/utils/impl/GnuLinuxResourceUtils.kt
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component
 class GnuLinuxResourceUtils : ResourceUtils {
     override fun getResources(): StaticResourcesDto {
         val nProc = "nproc".runCommand()?.replace("\\s".toRegex(), "")
-        val ram = arrayOf("bash", "-c", "free --mega | grep Mem | awk '{ print \$2 }'").runCommand()?.replace("\\s".toRegex(), "")
+        val ram = arrayOf("sh", "-c", "free --mega | grep Mem | awk '{ print \$2 }'").runCommand()?.replace("\\s".toRegex(), "")
         return StaticResourcesDto(logicalCpu = nProc?.toInt(), ramM = ram?.toInt())
     }
 }
